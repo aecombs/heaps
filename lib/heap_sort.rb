30046829c005767c349b0b6e0bcd741f93a3f3fe
Alicia Combs <alicia.combs.92@gmail.com>
2020-09-26 00:03:03 -0700

    Prelim implementation of heapsort

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -1,8 +1,17 @@
-
+require_relative './min_heap'
 
 # This method uses a heap to sort an array.
-# Time Complexity:  ?
-# Space Complexity: ?
-def heap_sort(list)
-  raise NotImplementedError, "Method not implemented yet..."
+# Time Complexity:  O(n)
+# Space Complexity: O(1)
+def heapsort(list)
+  return list if list.empty? || list.length == 1
+
+  #like insertion sort...hm!
+  heap = MinHeap.new
+
+  list.each do |element|
+    heap.add(element.to_s)
+  end
+
+  return heap
 end
